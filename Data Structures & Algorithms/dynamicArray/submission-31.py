893e7a0d8e5f32b9535f7e1d6b27dba16edb9c1c
Juliano Silva <juliano.silva2011@gmail.com>
2026-07-29 20:24:36 -0300

    Add: dynamicArray - submission-31

---
+++ Data Structures & Algorithms/dynamicArray/submission-31.py
@@ -0,0 +1,37 @@
+class DynamicArray:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.values = [0] * capacity
+        self.size = 0
+
+
+    def get(self, i: int) -> int:
+        return self.values[i]
+
+    def set(self, i: int, n: int) -> None:
+        self.values[i] = n
+
+    def pushback(self, n: int) -> None:
+        if self.size == self.capacity:
+            self.resize()
+
+        self.values[self.size] = n
+        self.size += 1
+
+    def popback(self) -> int:
+        last_value = self.values[self.size - 1]
+        self.size -= 1
+        return last_value
+
+
+    def resize(self) -> None:
+        self.values = self.values + ([0] * self.capacity * 2)
+        self.capacity = self.capacity * 2
+
+
+    def getSize(self) -> int:
+        return self.size
+        
+    
+    def getCapacity(self) -> int:
+        return self.capacity
